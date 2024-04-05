a7240b532596e1c8d88c8a9f7776d29323364fb7
hdert <40216616+hdert@users.noreply.github.com>
2024-04-05 21:04:56 +1300

    DataProcessor: add header to processed data files

--- src/DataProcessor/main.py
+++ src/DataProcessor/main.py
@@ -5,6 +5,7 @@ import datetime
 from os import path
 import numpy as np
 import calendar
+from io import TextIOWrapper
 
 
 def get_data(file_path: str) -> {int: [np.array]}:
@@ -38,8 +39,15 @@ def construct_averages(data: {int: [np.array]}) -> {int: [int]}:
     return result
 
 
+def write_header(data_file: TextIOWrapper) -> None:
+    data_file.write(
+        "Day, 00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23\n"
+    )
+
+
 def save_averages(file_path: str, data: {int: [int]}) -> None:
     with open(file_path, "w") as analysis_file:
+        write_header(analysis_file)
         writer = csv.writer(analysis_file, delimiter=",")
         for day, day_data in data.items():
             writer.writerow([calendar.day_abbr[day]] + day_data)
