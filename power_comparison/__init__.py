2420283e48effc934b6dee315b023f760fc0e182
hdert <40216616+hdert@users.noreply.github.com>
2024-08-10 16:36:33 +1200

    Delete unneeded files

--- power_comparison/__init__.py
+++ power_comparison/__init__.py
@@ -1,4 +1,4 @@
 # from .config import Config
-from .connector_utility import ConnectorUtility
+# from .connector_utility import ConnectorUtility
 from .default_values_utility import DefaultValuesUtility as DVU
 from .connectors import Connectors
