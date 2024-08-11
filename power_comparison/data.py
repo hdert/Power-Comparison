85c96f508768886b30f37274516789da17f8575b
hdert <40216616+hdert@users.noreply.github.com>
2024-08-11 19:57:16 +1200

    change DB table format, add data ingesting and other data functions

--- power_comparison/data.py
+++ power_comparison/data.py
@@ -1,88 +1,164 @@
 """Define the Data class."""
 
 import sqlite3
-from datetime import date
+from datetime import date, timedelta
 from .default_values_utility import DefaultValuesUtility as DVU
 
 
 class Data:
     """Hold usage data, and manipulation tools."""
 
-    def __init__(self) -> None:
+    _username: str
+    _user_id: int
+
+    def __init__(self, username: str) -> None:
         """Initialize the Data object."""
         db_filepath = DVU.get_db_file_path()
-        DVU.create_dirs(db_filepath)
+        DVU.create_dirs(db_filepath, filepath=True)
+        self._username = username
         self.connection = sqlite3.connect(db_filepath)
         self.cursor = self.connection.cursor()
         self.initialize_database()
 
-    def initialize_database(self) -> None:
-        """Ensure database is initialized and table exists."""
+    def initialize_user(self) -> None:
+        """Ensure user is initialized."""
         result = self.cursor.execute(
-            "SELECT name FROM sqlite_master WHERE name='usage_data'"
+            "SELECT username_email FROM user_data WHERE username_email=?",
+            (self._username,),
         )
-        if result.fetchone() is not None:
+        row = result.fetchone()
+        if row is not None:
+            self._user_id = row[0]
             return
         self.cursor.execute(
-            """CREATE TABLE
-            user_data(
-               user_id NOT NULL PRIMARY KEY,
-               username_email TEXT
-            )"""
+            """INSERT INTO user_data (username_email)
+        VALUES(?)""",
+            (self._username,),
         )
-        self.cursor.execute(
-            """CREATE TABLE
-            usage_data(
-                user_id INTEGER NOT NULL,
-                date INTEGER NOT NULL,
-                day INTEGER NOT NULL,
-                hour_0 INTEGER NOT NULL,
-                hour_1 INTEGER NOT NULL,
-                hour_2 INTEGER NOT NULL,
-                hour_3 INTEGER NOT NULL,
-                hour_4 INTEGER NOT NULL,
-                hour_5 INTEGER NOT NULL,
-                hour_6 INTEGER NOT NULL,
-                hour_7 INTEGER NOT NULL,
-                hour_8 INTEGER NOT NULL,
-                hour_9 INTEGER NOT NULL,
-                hour_10 INTEGER NOT NULL,
-                hour_11 INTEGER NOT NULL,
-                hour_12 INTEGER NOT NULL,
-                hour_13 INTEGER NOT NULL,
-                hour_14 INTEGER NOT NULL,
-                hour_15 INTEGER NOT NULL,
-                hour_16 INTEGER NOT NULL,
-                hour_17 INTEGER NOT NULL,
-                hour_18 INTEGER NOT NULL,
-                hour_19 INTEGER NOT NULL,
-                hour_20 INTEGER NOT NULL,
-                hour_21 INTEGER NOT NULL,
-                hour_22 INTEGER NOT NULL,
-                hour_23 INTEGER NOT NULL,
-                PRIMARY KEY (user_id, date),
-                FOREIGN KEY (user_id)
-                    REFERENCES user_data (user_id)
-            )"""
+        result = self.cursor.execute(
+            "SELECT user_id FROM user_data WHERE username_email=?",
+            (self._username,),
         )
-        self.connection.commit()
+        self._user_id = result.fetchone()[0]
 
-    def get_last_date(self, username: str) -> date | None:
+    def initialize_database(self) -> None:
+        """Ensure database is initialized and table and user exist."""
+        try:
+            result = self.cursor.execute(
+                "SELECT name FROM sqlite_master WHERE name='usage_data'"
+            )
+            if result.fetchone() is not None:
+                return
+            self.cursor.execute(
+                """CREATE TABLE
+                user_data(
+                user_id PRIMARY KEY,
+                username_email TEXT
+                )"""
+            )
+            self.cursor.execute(
+                """CREATE TABLE
+                usage_data(
+                    user_id INTEGER NOT NULL,
+                    date INTEGER NOT NULL, -- Gregorian Ordinal day
+                    day INTEGER NOT NULL, -- 0 index day of week
+                    hour INTEGER NOT NULL, -- 0 index hour of day
+                    value REAL NOT NULL,
+                    PRIMARY KEY (user_id, date, hour),
+                    FOREIGN KEY (user_id)
+                        REFERENCES user_data (user_id)
+                )"""
+            )
+        finally:
+            self.initialize_user()
+            self.connection.commit()
+
+    def get_last_date(self) -> date | None:
         """Return usage data date, or None."""
         result = self.cursor.execute(
             """SELECT date
             FROM usage_data
-            WHERE username_email = ?
-            LEFT JOIN user_data
-            ON usage_data.user_id = user_data.user_id
+            WHERE user_id = ?
             ORDER BY date DESC
             """,
-            username,
+            (self._user_id,),
         )
         row = result.fetchone()
         if row is None:
             return None
-        return date.fromtimestamp(row[0])
+        return date.fromordinal(row[0])
+
+    def ingest_data(self, data: list[tuple[date, list[float]]]) -> None:
+        """Ingest data."""
+        for data_date, values in data:
+            data_date_ord = data_date.toordinal()
+            day = data_date.weekday()
+            self.cursor.executemany(
+                """INSERT INTO usage_data VALUES(?)""",
+                (
+                    (self._user_id, data_date_ord, day, hour, value)
+                    for hour, value in enumerate(values)
+                ),
+            )
+        self.connection.commit()
+
+    def get_average_usage(
+        self, start_date: date | None = None, end_date: date | None = None
+    ) -> list[list[float]] | None:
+        """Get average of usage data for every hour of every weekday.
+
+        start_date:
+            Defaults to one year ago. The first date to include.
+        end_date:
+            Defaults to today. The last date to include."""
+        if end_date is None:
+            end_date = date.today()
+        if start_date is None:
+            start_date = end_date - timedelta(days=365)
+        result = self.cursor.execute(
+            """SELECT AVG(value), day, hour
+            FROM usage_data
+            WHERE user_id = ?
+            AND date > ?
+            AND date < ?
+            GROUP BY (day, hour)
+            ORDER BY day ASC, hour ASC""",
+            (self._user_id, start_date.toordinal(), end_date.toordinal()),
+        )
+        data = result.fetchall()
+        if len(data) == 0:
+            return None
+        if len(data) != 7 * 24:
+            print("WARNING: Data not right size.")
+        return [
+            [row[0] for row in data[i * 24 : (i + 1) * 24]] for i in range(7)
+        ]
+
+    def get_usage_per_weekday(
+        self, start_date: date | None = None, end_date: date | None = None
+    ) -> list[float] | None:
+        """Get average usage data per weekday.
+
+        start_date:
+            Defaults to one year ago. The first date to include.
+        end_date:
+            Defaults to today. The last date to include."""
+        if end_date is None:
+            end_date = date.today()
+        if start_date is None:
+            start_date = end_date - timedelta(days=365)
+        result = self.cursor.execute(
+            """SELECT AVG(value), hour
+            FROM usage_data
+            WHERE user_id = ?
+            AND date > ?
+            AND date < ?
+            GROUP BY hour
+            ORDER BY hour ASC""",
+            (self._user_id, start_date.toordinal(), end_date.toordinal()),
+        )
+        data = result.fetchall()
+        return [row[0] for row in data]
 
     def close(self) -> None:
         """Close Data."""
