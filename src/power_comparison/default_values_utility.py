05b23b3abdba82b8a3c877b649dfe921443259fe
hdert <40216616+hdert@users.noreply.github.com>
2024-08-16 12:16:42 +1200

    Move files around to work with package format

--- src/power_comparison/default_values_utility.py
+++ src/power_comparison/default_values_utility.py
@@ -7,7 +7,7 @@ class DefaultValuesUtility:
     """Define utility functions for default values."""
 
     _PREFIX_DIR = "PowerComparison-UserData/"
-    _PROFILES_DIR = "profiles/"
+    _PROFILES_DIR = "profiles"
     _DB_FILE_PATH = "data/user_data.db"
 
     @staticmethod
@@ -21,7 +21,7 @@ class DefaultValuesUtility:
     @staticmethod
     def get_profiles_dir() -> str:
         """Return a path to the profiles directory."""
-        return DefaultValuesUtility._PROFILES_DIR
+        return str(Path(__file__).parent / DefaultValuesUtility._PROFILES_DIR)
 
     @staticmethod
     def get_db_file_path() -> str:
